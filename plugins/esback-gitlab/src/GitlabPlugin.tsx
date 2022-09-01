67bce04ce423d05b89da57b94dc18df36aec216b
Daniel Bravo <dbravo@vmware.com>
2022-09-01 17:11:52 -0500

    ESBACK-24 Add config options for gitlab plugin

--- plugins/esback-gitlab/src/GitlabPlugin.tsx
+++ plugins/esback-gitlab/src/GitlabPlugin.tsx
@@ -1,35 +1,86 @@
 import React from 'react'
-import { AppPluginInterface } from "@esback/core"
+import { AppPluginInterface, RoutableConfig } from "@esback/core"
 import { EntityLayout, EntitySwitch } from "@backstage/plugin-catalog"
 import { Grid } from '@material-ui/core';
 import {
-    isGitlabAvailable,
-    EntityGitlabContent,
-    EntityGitlabLanguageCard,
-    EntityGitlabContributorsCard,
-  } from '@loblaw/backstage-plugin-gitlab';
+  isGitlabAvailable,
+  EntityGitlabContent,
+  EntityGitlabLanguageCard,
+  EntityGitlabContributorsCard,
+  EntityGitlabMergeRequestsTable,
+  EntityGitlabMergeRequestStatsCard,
+  EntityGitlabPipelinesTable,
+} from '@loblaw/backstage-plugin-gitlab';
 
-export const GitlabPlugin: AppPluginInterface = () => {
+interface GitlabConfig {
+  tab?: boolean
+  overview?: {
+    contributors?: boolean
+    language?: boolean
+    mrs?: boolean
+    mrStats?: boolean
+    pipelines?: boolean
+  }
+}
+
+export const GitlabPlugin: AppPluginInterface<GitlabConfig & RoutableConfig> = (config) => {
+  const { label, path, tab } = {
+    tab: true,
+    label: "Gitlab",
+    path: "gitlab",
+    ...config
+  }
+
+  const { contributors, language, mrs, mrStats, pipelines } = config?.overview ?? {
+    language: true,
+    mrStats: true,
+  }
+
   return {
     entityPage: (surface) => {
-      surface.addServicePageTab(
-        <EntityLayout.Route if={isGitlabAvailable} path="/gitlab" title="Gitlab">
-          <EntityGitlabContent />
-        </EntityLayout.Route>
-      )
+      if (tab) {
+        surface.addServicePageTab(
+          <EntityLayout.Route if={isGitlabAvailable} path={`/${path}`} title={label}>
+            <EntityGitlabContent />
+          </EntityLayout.Route>
+        )
+      }
 
-      surface.addOverviewContent(
-        <EntitySwitch>
-          <EntitySwitch.Case if={isGitlabAvailable}>
-            <Grid item md={6}>
-              <EntityGitlabContributorsCard />
-            </Grid>
-            <Grid item md={6}>
-              <EntityGitlabLanguageCard />
-            </Grid>
-          </EntitySwitch.Case>
-        </EntitySwitch>
-      )
+      const gridItems = []
+
+      if (contributors) {
+        gridItems.push(<EntityGitlabContributorsCard />)
+      }
+
+      if (language) {
+        gridItems.push(<EntityGitlabLanguageCard />)
+      }
+
+      if (mrs) {
+        gridItems.push(<EntityGitlabMergeRequestsTable />)
+      }
+
+      if (mrStats) {
+        gridItems.push(<EntityGitlabMergeRequestStatsCard />)
+      }
+
+      if (pipelines) {
+        gridItems.push(<EntityGitlabPipelinesTable />)
+      }
+
+      if (gridItems.length > 0) {
+        surface.addOverviewContent(
+          <EntitySwitch>
+            <EntitySwitch.Case if={isGitlabAvailable}>
+            {gridItems.map(gitlabCard => (
+              <Grid item md={6}>
+                {gitlabCard}
+              </Grid>
+            ))}
+            </EntitySwitch.Case>
+          </EntitySwitch>
+        )
+      }
     }
   }
 }
