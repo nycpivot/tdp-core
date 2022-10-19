33673bea1ec2afbb91549433ebcd54d17bfe627f
Fouad Hamdi <fhamdi@vmware.com>
2022-10-19 13:52:57 +0200

    ESBACK-60 Prettify integration tests

--- packages/app/cypress/integration/app.js
+++ packages/app/cypress/integration/app.js
@@ -1,8 +1,8 @@
 describe('App', () => {
-  describe("Catalog Plugin", () => {
+  describe('Catalog Plugin', () => {
     beforeEach(() => {
       cy.visit('/');
-    })
+    });
 
     it('should render the title', () => {
       cy.contains('VMware Catalog');
@@ -10,17 +10,17 @@ describe('App', () => {
 
     it('should render the local catalog', () => {
       cy.contains('example-website');
-    })
+    });
 
     it('should render the gitlab catalog', () => {
       cy.contains('tap-gui-component');
-    })
-  })
+    });
+  });
 
-  describe("Hello World Plugin", () => {
+  describe('Hello World Plugin', () => {
     it('should have the hello-world plugin', () => {
       cy.visit('/hello-world');
       cy.contains('Hello World!!');
     });
-  })
+  });
 });
