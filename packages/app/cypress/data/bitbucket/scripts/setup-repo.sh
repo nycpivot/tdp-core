e0d44c43b0ccc8bdbec2edac0403ca5e0cdadc20
Fouad Hamdi <fhamdi@vmware.com>
2022-10-26 17:07:38 +0200

    ESBACK-75 Create a repository in the Bitbucket server

--- packages/app/cypress/data/bitbucket/scripts/setup-repo.sh
+++ packages/app/cypress/data/bitbucket/scripts/setup-repo.sh
@@ -2,20 +2,45 @@
 set -euo pipefail
 
 BASE_URL="http://localhost:7990"
+PROJECT_KEY=ESBACK
+REPO=catalog
 
-echo "Create a project"
+echo "Creating a project..."
 echo
 
 curl --request POST \
-  --url "{$BASE_URL}/rest/api/latest/projects" \
+  --url "$BASE_URL/rest/api/latest/projects" \
   --user esback:esback \
   --header 'Accept: application/json' \
   --header 'Content-Type: application/json' \
-  --data '{
-  "key": "ESBACK",
-  "name": "esback"
-}'
+  --data "{
+  \"key\": \"$PROJECT_KEY\",
+  \"name\": \"esback\"
+}"
 
+echo
+echo
+echo "Project $PROJECT_KEY created."
+echo
+
+echo "Creating a repository..."
+echo
+
+curl --request POST \
+  --url "$BASE_URL/rest/api/latest/projects/$PROJECT_KEY/repos" \
+  --user esback:esback \
+  --header 'Accept: application/json' \
+  --header 'Content-Type: application/json' \
+  --data "{
+  \"name\": \"catalog\",
+  \"scmId\": \"git\",
+  \"slug\": \"$REPO\"
+}"
+
+echo
+echo
+echo "Repository $REPO created."
+echo
 
 echo
 echo
