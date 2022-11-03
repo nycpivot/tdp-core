ad5637c4f5aa90a59db832c2319fc083a96b7595
Fouad Hamdi <fhamdi@vmware.com>
2022-11-03 14:28:33 +0100

    ESBACK-75 Add a script to run the tests locally

--- packages/app/cypress/scripts/library.sh
+++ packages/app/cypress/scripts/library.sh
@@ -1,8 +1,23 @@
-MSG_COLOR="\e[1;32m"
-DATA_COLOR="\e[1;35m"
-RESET_MARKER="\e[0m"
+# Little trick to be able to exit from script when a function is called with the $(...) syntax
+trap "exit 1" TERM
+export TOP_PID=$$
+
+MSG_COLOR="\033[1;32m"
+ERROR_COLOR="\033[1;31m"
+DATA_COLOR="\033[1;35m"
+RESET_MARKER="\033[0m"
 
 print_message() {
-  echo -e "${MSG_COLOR}$1${RESET_MARKER}"
+  echo -e "${MSG_COLOR}${1}${RESET_MARKER}"
   echo
+} >&2
+
+print_error() {
+  echo -e "${ERROR_COLOR}${1}${RESET_MARKER}"
+  echo
+} >&2
+
+fail() {
+  print_error "${1}"
+  kill -s TERM $TOP_PID
 }
--- packages/app/cypress/scripts/run-tests.sh
+++ packages/app/cypress/scripts/run-tests.sh
@@ -7,7 +7,7 @@ print_message "Installing typescript..."
 npm install typescript@^4.4.3
 print_message "Typescript installed"
 
-print_message "Waiting for esback server..."
+print_message "Waiting for esback server (it might take a moment)..."
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' $CYPRESS_baseUrl)" != "200" ]]; do sleep 5; done
 print_message "esback is ready !"
 
