2e82531af19310fab69b2e6e5ade107e5f75d6dc
Fouad Hamdi <fhamdi@vmware.com>
2022-11-03 09:54:25 +0100

    ESBACK-75 Colorize the e2e scripts

--- packages/app/cypress/scripts/bitbucket/setup.sh
+++ packages/app/cypress/scripts/bitbucket/setup.sh
@@ -1,62 +1,95 @@
 #!/bin/bash
 set -euo pipefail
 
+MSG_COLOR="\e[1;32m"
+DATA_COLOR="\e[1;35m"
+RESET_MARKER="\e[0m"
+
 BASE_URL=${BASE_URL:-"http://localhost:7990"}
 PROJECT_KEY=ESBACK
 REPO=catalog
 REPO_FOLDER=${REPO_FOLDER:-"./../data/bitbucket/repo"}
 
-echo "Creating a project..."
-echo
+echo -e "${MSG_COLOR}Checking if project already exists..."
+status_code=$(curl -s -o /dev/null -u esback:esback -w ''%{http_code}'' $BASE_URL/rest/api/1.0/projects/$PROJECT_KEY)
 
-curl --silent --request POST \
-  --url "$BASE_URL/rest/api/latest/projects" \
-  --user esback:esback \
-  --header 'Accept: application/json' \
-  --header 'Content-Type: application/json' \
-  --data "{
-  \"key\": \"$PROJECT_KEY\",
-  \"name\": \"esback\"
-}"
+if [[ $status_code != "200" ]]
+then
+  echo -e "${MSG_COLOR}Creating a project...${RESET_MARKER}"
+    echo
+
+    curl --silent --request POST \
+      --url "$BASE_URL/rest/api/latest/projects" \
+      --user esback:esback \
+      --header 'Accept: application/json' \
+      --header 'Content-Type: application/json' \
+      --data "{
+      \"key\": \"$PROJECT_KEY\",
+      \"name\": \"esback\"
+    }"
+
+    echo
+    echo
+    echo -e "${MSG_COLOR}Project ${DATA_COLOR}$PROJECT_KEY created.${RESET_MARKER}"
+    echo
+else
+  echo -e "${MSG_COLOR}Project already exists.${RESET_MARKER}"
+  echo
+fi
+
+
+echo -e "${MSG_COLOR}Checking if repository already exists...${RESET_MARKER}"
+status_code=$(curl -s -o /dev/null -u esback:esback -w ''%{http_code}'' $BASE_URL/rest/api/1.0/projects/$PROJECT_KEY/repos/${REPO})
+
+if [[ $status_code != "200" ]]
+then
+  echo -e "${MSG_COLOR}Creating a repository...${RESET_MARKER}"
+  echo
+
+  curl --silent --request POST \
+    --url "$BASE_URL/rest/api/latest/projects/$PROJECT_KEY/repos" \
+    --user esback:esback \
+    --header 'Accept: application/json' \
+    --header 'Content-Type: application/json' \
+    --data "{
+    \"name\": \"catalog\",
+    \"scmId\": \"git\",
+    \"slug\": \"$REPO\"
+  }"
+
+  echo
+  echo
+  echo -e "${MSG_COLOR}Repository ${DATA_COLOR}$REPO created.${RESET_MARKER}"
+  echo
+else
+  echo -e "${MSG_COLOR}Repository already exists.${RESET_MARKER}"
+  echo
+fi
+
+echo -e "${MSG_COLOR}Checking if catalog already exists...${RESET_MARKER}"
+
+status_code=$(curl -s -o /dev/null -u esback:esback -w ''%{http_code}'' $BASE_URL/rest/api/1.0/projects/$PROJECT_KEY/repos/${REPO}/raw/catalog-info.yaml)
+
+if [[ $status_code != "200" ]]
+then
+  echo -e "${MSG_COLOR}Adding a catalog in the repository...${RESET_MARKER}"
+  echo
+  curl --silent --request PUT \
+    --url "$BASE_URL/rest/api/latest/projects/$PROJECT_KEY/repos/$REPO/browse/catalog-info.yaml" \
+    --user esback:esback \
+    -F content=@$REPO_FOLDER/catalog-info.yaml \
+    -F branch=master \
+    -F 'message=Add catalog info'
+
+  echo
+  echo
+  echo -e "${MSG_COLOR}Catalog added${RESET_MARKER}"
+else
+    echo -e "${MSG_COLOR}Catalog already exists.${RESET_MARKER}"
+    echo
+fi
+
 
 echo
 echo
-echo "Project $PROJECT_KEY created."
-echo
-
-echo "Creating a repository..."
-echo
-
-curl --silent --request POST \
-  --url "$BASE_URL/rest/api/latest/projects/$PROJECT_KEY/repos" \
-  --user esback:esback \
-  --header 'Accept: application/json' \
-  --header 'Content-Type: application/json' \
-  --data "{
-  \"name\": \"catalog\",
-  \"scmId\": \"git\",
-  \"slug\": \"$REPO\"
-}"
-
-echo
-echo
-echo "Repository $REPO created."
-echo
-
-echo "Adding a catalog in the repository..."
-echo
-
-curl --silent --request PUT \
-  --url "$BASE_URL/rest/api/latest/projects/$PROJECT_KEY/repos/$REPO/browse/catalog-info.yaml" \
-  --user esback:esback \
-  -F content=@$REPO_FOLDER/catalog-info.yaml \
-  -F branch=master \
-  -F 'message=Add catalog info'
-
-echo
-echo
-echo "Catalog added"
-
-echo
-echo
-echo "That's All, Folks!"
+echo -e "${MSG_COLOR}That's All, Folks!${RESET_MARKER}"
--- packages/app/cypress/scripts/run-tests.sh
+++ packages/app/cypress/scripts/run-tests.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 set -euo pipefail
 
-echo "Installing typescript..."
+MSG_COLOR="\e[1;32m"
+DATA_COLOR="\e[1;35m"
+RESET_MARKER="\e[0m"
+
+echo -e "${MSG_COLOR}Installing typescript...${RESET_MARKER}"
 npm install typescript@^4.4.3
 echo
-echo "Typescript installed"
+echo -e "${MSG_COLOR}Typescript installed${RESET_MARKER}"
 echo
 
-echo "Waiting for esback server..."
+echo -e "${MSG_COLOR}Waiting for esback server...${RESET_MARKER}"
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' $CYPRESS_baseUrl)" != "200" ]]; do sleep 5; done
 echo
-echo "esback ready !"
+echo -e "${MSG_COLOR}esback ready !${RESET_MARKER}"
 echo
-
 
 cypress run --browser chrome
