7521334c8add81df72e769306f151efccb3aa45b
Fouad Hamdi <fhamdi@vmware.com>
2022-11-03 09:26:50 +0100

    ESBACK-75 Colorize the e2e scripts

--- packages/app/cypress/scripts/start-esback.sh
+++ packages/app/cypress/scripts/start-esback.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 set -euo pipefail
 
-echo "Installing jq & curl..."
+MSG_COLOR="\e[1;32m"
+DATA_COLOR="\e[1;35m"
+RESET_MARKER="\e[0m"
+
+echo -e "${MSG_COLOR}Installing jq & curl...${RESET_MARKER}"
 apt update
 apt install -y curl jq
 echo
-echo "jq & curl installed."
+echo -e "${MSG_COLOR}jq & curl installed.${RESET_MARKER}"
 echo
 
-echo "Waiting for Bitbucket..."
+echo -e "${MSG_COLOR}Waiting for Bitbucket...${RESET_MARKER}"
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' -u esback:esback http://$BITBUCKET_HOST/rest/api/latest/projects)" != "200" ]]; do sleep 5; done
 echo
-echo "Bitbucket ready !"
+echo -e "${MSG_COLOR}Bitbucket ready \!${RESET_MARKER}"
 echo
 
-echo "Running Bitbucket setup..."
+echo -e "${MSG_COLOR}Running Bitbucket setup...${RESET_MARKER}"
 BASE_URL=http://$BITBUCKET_HOST REPO_FOLDER=/e2e/cypress/data/bitbucket/repo /e2e/cypress/scripts/bitbucket/setup.sh
 echo
-echo "Bitbucket setup done"
+echo -e "${MSG_COLOR}Bitbucket setup done${RESET_MARKER}"
 echo
 
-echo "Generating Bitbucket token..."
+echo -e "${MSG_COLOR}Generating Bitbucket token...${RESET_MARKER}"
 bitbucket_token=$(BASE_URL=http://$BITBUCKET_HOST /e2e/cypress/scripts/bitbucket/generate-token.sh)
 echo
-echo "Generated token: $bitbucket_token"
+echo -e "${MSG_COLOR}Generated token: ${DATA_COLOR}$bitbucket_token${RESET_MARKER}"
 echo
 
 export BITBUCKET_TOKEN="$bitbucket_token"
