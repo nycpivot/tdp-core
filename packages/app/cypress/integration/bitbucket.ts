3af0ada3d192fe1a014b823ab1763997df6abb2f
Fouad Hamdi <fhamdi@vmware.com>
2022-11-08 11:53:28 +0100

    ESBACK-91 Add bitbucket dependency

--- packages/app/cypress/integration/bitbucket.ts
+++ packages/app/cypress/integration/bitbucket.ts
@@ -1,5 +1,11 @@
+import * as BitbucketServer from "@atlassian/bitbucket-server"
+
 describe("Bitbucket Server", () => {
-  it('should render the bitbucket server catalog', () => {
+  it('should render the bitbucket server catalog', async () => {
+    const bitbucket = new BitbucketServer({
+      baseUrl: `http://${process.env.BITBUCKET_HOST}`
+    })
+    bitbucket.authenticate({ type: 'basic', username: 'esback', password: 'esback' })
     cy.visit('/');
     cy.contains('bitbucket-server-component');
   });
--- packages/app/cypress/scripts/run-tests.sh
+++ packages/app/cypress/scripts/run-tests.sh
@@ -7,4 +7,4 @@ print_message "Waiting for esback server (it might take a moment)..."
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' ${CYPRESS_baseUrl})" != "200" ]]; do sleep 5; done
 print_message "esback is ready !"
 
-npx cypress run --browser chrome
+BITBUCKET_HOST=${BITBUCKET_HOST} npx cypress run --browser chrome
