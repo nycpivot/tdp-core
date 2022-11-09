e96b709f415b505aeb4ab9880e9dc759457102f1
Dimitry Besson <dbesson@vmware.com>
2022-11-09 11:50:31 -0500

    add providers to BackendCatalogSurface
    [ESBACK-92]

--- packages/core/src/surfaces/BackendCatalogSurface.ts
+++ packages/core/src/surfaces/BackendCatalogSurface.ts
@@ -1,17 +1,30 @@
-import { CatalogProcessor } from '@backstage/plugin-catalog-backend';
+import {
+  CatalogProcessor,
+  EntityProvider,
+} from '@backstage/plugin-catalog-backend';
 
 export class BackendCatalogSurface {
   private readonly _processors: CatalogProcessor[];
+  private readonly _providers: EntityProvider[];
 
   constructor() {
     this._processors = [];
+    this._providers = [];
   }
 
   public addCatalogProcessor(processor: CatalogProcessor) {
     this._processors.push(processor);
   }
 
+  public addEntityProvider(provider: EntityProvider) {
+    this._providers.push(provider);
+  }
+
   public get processors(): CatalogProcessor[] {
     return this._processors;
+  }
+
+  public get providers(): EntityProvider[] {
+    return this._providers;
   }
 }
