91b2b18d2e62e9546c118b6c6b57306f33d8c51e
Fouad Hamdi <fhamdi@vmware.com>
2022-11-10 15:34:11 +0100

    ESBACK-100 Prettify

--- plugins/esback-catalog-backend/src/CatalogBackendPlugin.test.ts
+++ plugins/esback-catalog-backend/src/CatalogBackendPlugin.test.ts
@@ -1,51 +1,59 @@
-import {CatalogBackendPlugin} from "./CatalogBackendPlugin";
+import { CatalogBackendPlugin } from './CatalogBackendPlugin';
 import {
   BackendCatalogSurface,
   BackendPluginInterface,
   BackendPluginSurface,
   PluginEnvironment,
-  SurfaceStore
-} from "@esback/core";
-import {ConfigReader} from '@backstage/config';
+  SurfaceStore,
+} from '@esback/core';
+import { ConfigReader } from '@backstage/config';
 import {
   CacheManager,
   DatabaseManager,
   FetchUrlReader,
   getVoidLogger,
-  ServerTokenManager
-} from "@backstage/backend-common";
-import {AuthorizeResult, DefinitivePolicyDecision, PermissionEvaluator} from "@backstage/plugin-permission-common";
+  ServerTokenManager,
+} from '@backstage/backend-common';
+import {
+  AuthorizeResult,
+  DefinitivePolicyDecision,
+  PermissionEvaluator,
+} from '@backstage/plugin-permission-common';
 import request from 'supertest';
-import express from "express";
-import {DeferredEntity} from "@backstage/plugin-catalog-backend";
-import {TaskScheduler} from "@backstage/backend-tasks";
+import express from 'express';
+import { DeferredEntity } from '@backstage/plugin-catalog-backend';
+import { TaskScheduler } from '@backstage/backend-tasks';
 
-describe("Catalog Backend Plugin", () => {
+describe('Catalog Backend Plugin', () => {
   it('should return entities', async () => {
     const router = await createPluginRouter([fakeEntityProviderPlugin()]);
     const app = express();
     app.use(router);
     await waitForStitching();
 
-    const res = await request(app).get('/entities')
+    const res = await request(app).get('/entities');
 
     expect(res.statusCode).toBe(200);
     expect(res.body).toHaveLength(1);
-    expect(res.body[0].kind).toBe("Component");
-    expect(res.body[0].metadata.name).toBe("fake-entity");
-  })
+    expect(res.body[0].kind).toBe('Component');
+    expect(res.body[0].metadata.name).toBe('fake-entity');
+  });
 
-  async function createPluginRouter(catalogInternalPlugins: BackendPluginInterface[]) {
+  async function createPluginRouter(
+    catalogInternalPlugins: BackendPluginInterface[],
+  ) {
     const plugin = createCatalogPlugin(catalogInternalPlugins);
-    const router = await plugin.pluginFn(pluginEnvironment())
+    const router = await plugin.pluginFn(pluginEnvironment());
     return router;
   }
 
-  function createCatalogPlugin(catalogInternalPlugins: BackendPluginInterface[]) {
+  function createCatalogPlugin(
+    catalogInternalPlugins: BackendPluginInterface[],
+  ) {
     const catalogPlugin = CatalogBackendPlugin();
     const store = new SurfaceStore();
     catalogInternalPlugins.forEach(p => p()(store));
-    catalogPlugin(store)
+    catalogPlugin(store);
 
     const surface = store.getSurfaceState(BackendPluginSurface);
     return surface.plugins[0];
@@ -57,41 +65,44 @@ describe("Catalog Backend Plugin", () => {
     // and doesn't seem overridable from the tests hence the usage
     // of a timeout here (until we find a better way to trigger the
     // entities refresh in the database)
-    await new Promise(resolve => setTimeout(resolve, 1500))
+    await new Promise(resolve => setTimeout(resolve, 1500));
   }
 
   function fakeEntityProviderPlugin() {
     const entityProviderPlugin: BackendPluginInterface = () => surfaces =>
-      surfaces.applyTo(BackendCatalogSurface, surface => surface.addEntityProvider({
-        async connect(connection): Promise<void> {
-          const entity: DeferredEntity = {
-            entity: {
-              kind: 'Component',
-              apiVersion: "backstage.io/v1alpha1",
-              metadata: {
-                name: "fake-entity",
-                annotations: {
-                  'backstage.io/managed-by-location': 'url:https://host/path',
-                  'backstage.io/managed-by-origin-location': 'url:https://host/path',
+      surfaces.applyTo(BackendCatalogSurface, surface =>
+        surface.addEntityProvider({
+          async connect(connection): Promise<void> {
+            const entity: DeferredEntity = {
+              entity: {
+                kind: 'Component',
+                apiVersion: 'backstage.io/v1alpha1',
+                metadata: {
+                  name: 'fake-entity',
+                  annotations: {
+                    'backstage.io/managed-by-location': 'url:https://host/path',
+                    'backstage.io/managed-by-origin-location':
+                      'url:https://host/path',
+                  },
+                },
+                spec: {
+                  type: 'website',
+                  owner: 'esback',
+                  lifecycle: 'test',
                 },
               },
-              spec: {
-                type: 'website',
-                owner: 'esback',
-                lifecycle: 'test'
-              }
-            }
-          };
-          await connection.applyMutation({
-            type: 'full',
-            entities: [entity]
-          })
-          return Promise.resolve(undefined);
-        },
-        getProviderName(): string {
-          return "fake-provider";
-        }
-      }))
+            };
+            await connection.applyMutation({
+              type: 'full',
+              entities: [entity],
+            });
+            return Promise.resolve(undefined);
+          },
+          getProviderName(): string {
+            return 'fake-provider';
+          },
+        }),
+      );
     return entityProviderPlugin;
   }
 
@@ -100,44 +111,41 @@ describe("Catalog Backend Plugin", () => {
       backend: {
         database: {
           client: 'better-sqlite3',
-          connection: ':memory:'
-        }
-      }
+          connection: ':memory:',
+        },
+      },
     });
 
     function permissionEvaluator() {
-      return new class implements PermissionEvaluator {
+      return new (class implements PermissionEvaluator {
         // @ts-ignore
         authorize(requests, options) {
-          const decision: DefinitivePolicyDecision = {
-            result: AuthorizeResult.ALLOW
-          };
-          return Promise.resolve([decision]);
+          return Promise.resolve([]);
         }
 
         // @ts-ignore
         authorizeConditional(requests, options) {
           const decision: DefinitivePolicyDecision = {
-            result: AuthorizeResult.ALLOW
+            result: AuthorizeResult.ALLOW,
           };
           return Promise.resolve([decision]);
         }
-      };
+      })();
     }
 
     return {
       logger: getVoidLogger(),
       config: config,
-      database: DatabaseManager.fromConfig(config).forPlugin("catalog"),
+      database: DatabaseManager.fromConfig(config).forPlugin('catalog'),
       permissions: permissionEvaluator(),
-      cache: CacheManager.fromConfig(config).forPlugin("catalog"),
+      cache: CacheManager.fromConfig(config).forPlugin('catalog'),
       discovery: {
         getBaseUrl: jest.fn(),
-        getExternalBaseUrl: jest.fn()
+        getExternalBaseUrl: jest.fn(),
       },
       reader: new FetchUrlReader(),
-      scheduler: TaskScheduler.fromConfig(config).forPlugin("catalog"),
+      scheduler: TaskScheduler.fromConfig(config).forPlugin('catalog'),
       tokenManager: ServerTokenManager.noop(),
     };
   }
-})
+});
