49a0da36732988be624bb12e119f6a348a03ddb2
Fouad Hamdi <fhamdi@vmware.com>
2022-11-16 11:51:42 +0100

    ESBACK-82 Reference new locations for github e2e fixtures

--- packages/app/cypress/integration/app.js
+++ packages/app/cypress/integration/app.js
@@ -49,16 +49,6 @@ describe('App', () => {
     it('should render the component from the provider', () => {
       cy.contains('gitlab-provider-component');
     });
-  });
-
-  describe('Gitlab Backend Plugin', () => {
-    beforeEach(() => {
-      cy.visit('/');
-    });
-
-    it('should render the component from the provider', () => {
-      cy.contains('gitlab-provider-component');
-    });
 
     it('should render the component from the processor', () => {
       cy.contains('gitlab-processor-component');
