108a42e1c9afc340a9cb607e858830fd8c0eca43
Fouad Hamdi <fhamdi@vmware.com>
2022-11-17 10:39:15 +0100

    ESBACK-91 Create a bitbucket project & repo in the test

--- packages/app/cypress/integration/bitbucket.ts
+++ packages/app/cypress/integration/bitbucket.ts
@@ -1,15 +1,56 @@
+import {faker} from '@faker-js/faker'
+
 describe("Bitbucket Server", () => {
   beforeEach(async () => {
     cy.log('Creating bitbucket token')
-    cy.log('bitbucket url: ' + `http://${Cypress.env('BITBUCKET_HOST')}/rest/access-tokens/latest/users/esback`)
+    cy.log('bitbucket url: ' + `http://${Cypress.env('BITBUCKET_HOST')}/rest/api/latest/projects`)
+
+    const project = {
+      key: faker.random.alpha({count: 5, casing: 'upper'}),
+      name: faker.random.alphaNumeric(10)
+    }
+
+    await new Cypress.Promise((resolve, reject) => cy.request(
+      {
+        method: 'POST',
+        url: `http://${Cypress.env('BITBUCKET_HOST')}/rest/api/latest/projects`,
+        headers: {
+          'Authorization': 'Basic ZXNiYWNrOmVzYmFjaw==',
+          'User-Agent': 'test-agent',
+        },
+        body: project
+      }
+    ).then(() => resolve()))
+
+    cy.log("Project " + project.name + " created (key: " + project.key + ")")
+
+    const repo = {
+      slug: faker.random.alpha({count: 5, casing: 'upper'}),
+      name: faker.random.alphaNumeric(10),
+      scmId: "git"
+    }
+
+    cy.request(
+      {
+        method: 'POST',
+        url: `http://${Cypress.env('BITBUCKET_HOST')}/rest/api/latest/projects/${project.key}/repos`,
+        headers: {
+          Authorization: 'Basic ZXNiYWNrOmVzYmFjaw==',
+          'User-Agent': 'test-agent',
+        },
+        body: repo
+      }
+    )
+
+    cy.log("repository " + repo.name + " created (slug: " + repo.slug + ")")
+
+
 
     const token = await new Cypress.Promise((resolve) => cy.request({
       method: 'PUT',
-      url: `http://${Cypress.env('BITBUCKET_HOST')}/rest/access-tokens/latest/users/esback`,
+      url: `http://${Cypress.env('BITBUCKET_HOST')}/rest/access-tokens/1.0/users/esback`,
       headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Authorization': 'Basic ZXNiYWNrOmVzYmFjaw=='
+        Authorization: 'Basic ZXNiYWNrOmVzYmFjaw=='
       },
       body: {
         expiryDays: 2154,
@@ -20,7 +61,9 @@ describe("Bitbucket Server", () => {
         ]
       }
     }).then((response) => resolve(response.body.token)))
+
     cy.log("token: " + token)
+
   })
 
   it('should render the bitbucket server catalog', async () => {
