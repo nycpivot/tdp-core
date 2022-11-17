1b01420624d11b380851e007f454e5840a84d55a
Fouad Hamdi <fhamdi@vmware.com>
2022-11-17 09:15:15 +0100

    ESBACK-91 Generate a bitbucket token from the e2e test

--- packages/app/cypress/integration/bitbucket.ts
+++ packages/app/cypress/integration/bitbucket.ts
@@ -1,13 +1,35 @@
-import axios from 'axios'
-
 describe("Bitbucket Server", () => {
+  beforeEach(async () => {
+    cy.log('Creating bitbucket token')
+    cy.log('bitbucket url: ' + `http://${Cypress.env('BITBUCKET_HOST')}/rest/access-tokens/latest/users/esback`)
+
+    const token = await new Cypress.Promise((resolve) => cy.request({
+      method: 'PUT',
+      url: `http://${Cypress.env('BITBUCKET_HOST')}/rest/access-tokens/latest/users/esback`,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': 'Basic ZXNiYWNrOmVzYmFjaw=='
+      },
+      body: {
+        expiryDays: 2154,
+        name: "super token",
+        permissions: [
+          "REPO_ADMIN",
+          "PROJECT_ADMIN"
+        ]
+      }
+    }).then((response) => resolve(response.body.token)))
+    cy.log("token: " + token)
+  })
+
   it('should render the bitbucket server catalog', async () => {
-    await axios.post(`${Cypress.config().baseUrl}/api/catalog/locations`, {
-      type: 'url',
-      target: `http://${Cypress.env('BITBUCKET_HOST')}/projects/ESBACK/repos/catalog/raw/bitbucket-server-integration-component.yaml`
-    })
+    // await axios.post(`${Cypress.config().baseUrl}/api/catalog/locations`, {
+    //   type: 'url',
+    //   target: `http://${Cypress.env('BITBUCKET_HOST')}/projects/ESBACK/repos/catalog/raw/bitbucket-server-integration-component.yaml`
+    // })
 
     cy.visit('/');
-    cy.contains('bitbucket-server-component');
+    // cy.contains('bitbucket-server-component');
   });
 })
