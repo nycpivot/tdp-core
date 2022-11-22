9a8a4f3d9b9d984a0a8c1d948c87567dbaae66d4
Fouad Hamdi <fhamdi@vmware.com>
2022-11-22 10:50:27 +0100

    ESBACK-91 Convert bash code that waits for bitbucket to typescript

--- packages/app/cypress/scripts/src/bitbucket-server/generateToken.ts
+++ packages/app/cypress/scripts/src/bitbucket-server/generateToken.ts
@@ -1,18 +1,19 @@
 import axios from 'axios'
 
-export async function generateToken(): Promise<string> {
-  const host = process.env.BITBUCKET_HOST;
+const host = process.env.BITBUCKET_HOST;
+const auth = {
+  username: "esback",
+  password: "esback"
+}
+
+async function generateToken(): Promise<string> {
   const response = await axios.put(`http://${host}/rest/access-tokens/latest/users/esback`, {
       expiryDays: 2154,
       name: "an e2e token",
-      permissions: ["REPO_ADMIN",
-        "PROJECT_ADMIN"]
+      permissions: ["REPO_ADMIN", "PROJECT_ADMIN"]
     },
     {
-      auth: {
-        username: "esback",
-        password: "esback"
-      }
+      auth: auth
     })
 
   if (response.status !== 200) {
@@ -22,6 +23,39 @@ export async function generateToken(): Promise<string> {
   return response.data.token
 }
 
-generateToken()
+const sleepUntil = async (f: () => Promise<boolean>, timeoutMs: number) => {
+  return new Promise((resolve, reject) => {
+    const timeWas = new Date().getTime();
+    const wait = setInterval(async function() {
+      const ready = await f()
+      if (ready) {
+        clearInterval(wait);
+        resolve(undefined);
+      } else if (new Date().getTime() - timeWas > timeoutMs) { // Timeout
+        process.stderr.write(`rejected after ${new Date().getTime() - timeWas} ms\n`);
+        clearInterval(wait);
+        reject();
+      }
+    }, 5000);
+  });
+}
+
+async function bitbucketServerIsReady(): Promise<boolean> {
+  try {
+    const response = await axios.get(`http://${host}/status`)
+    if (response.status === 200) {
+      if (response.data.state === "RUNNING") {
+        return true
+      }
+    }
+  } catch (e) {
+    process.stderr.write(`Bitbucket Server apparently not ready yet: ${e}\n`)
+  }
+
+  return false
+}
+
+sleepUntil(bitbucketServerIsReady, 5*60*1000)
+  .then(generateToken)
   .then(token => process.stdout.write(token))
   .catch(error => process.stderr.write(error.toString()))
--- packages/app/cypress/scripts/start-esback.sh
+++ packages/app/cypress/scripts/start-esback.sh
@@ -3,18 +3,9 @@ set -euo pipefail
 
 source $(dirname "$0")/library.sh
 
-print_message "Installing curl..."
-apt update
-apt install -y curl
-echo
-print_message "curl installed."
-
-print_message "Waiting for Bitbucket..."
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' -u esback:esback http://${BITBUCKET_HOST}/rest/api/latest/projects)" != "200" ]]; do sleep 5; done
-print_message "Bitbucket ready !"
-
 cd /scripts
 yarn install
+print_message "Looking for bitbucket server token..."
 bitbucket_token=$(npx ts-node src/bitbucket-server/generateToken.ts)
 print_message "Generated token: ${DATA_COLOR}${bitbucket_token}"
 cd -
