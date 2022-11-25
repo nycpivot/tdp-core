b6d543e9297a1e03b9a7ff99db17fa9b902dbce6
Fouad Hamdi <fhamdi@vmware.com>
2022-11-25 14:07:27 +0100

    ESBACK-125 Rename catalog provider/processor plugins

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,6 +1,6 @@
 import { BackendRuntime } from '@esback/runtime-backend';
-import { plugin as catalogEntityProviderPlugin } from '@esback/plugin-catalog-entity-provider';
-import { plugin as catalogProcessorPlugin } from '@esback/plugin-catalog-processor';
+import { plugin as catalogEntityProviderPlugin } from '@esback/plugin-test-entity-provider';
+import { plugin as catalogProcessorPlugin } from '@esback/plugin-test-entity-processor';
 import { plugin as gitlabPlugin } from '@esback/plugin-gitlab-backend';
 
 new BackendRuntime([
