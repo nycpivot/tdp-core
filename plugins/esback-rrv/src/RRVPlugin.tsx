2891596b590269e9faa4915f5d2a2d92a041e60d
Fouad Hamdi <fhamdi@vmware.com>
2022-12-09 09:50:33 +0100

    ESBACK-122 Add a plugin interface for tap-gui rrv plugin

---
+++ plugins/esback-rrv/src/RRVPlugin.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {AppPluginInterface, AppRouteSurface,} from '@esback/core';
+import {WorkloadsContent} from "@esback/plugin-rrv";
+import {EntityLayout} from "@backstage/plugin-catalog";
+import {EntityPageSurface} from "@esback/plugin-catalog";
+
+export const RRVPlugin: AppPluginInterface = () => context => {
+  context.applyWithDependency(AppRouteSurface, EntityPageSurface,
+    (routes, surface) => {
+      surface.addServicePageTab(<EntityLayout.Route path="/workloads" title="Runtime Resources">
+        <WorkloadsContent/>
+      </EntityLayout.Route>)
+    })
+};
