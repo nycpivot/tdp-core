0a1c6f3f434bff9fe89af80f59ae5d0235f15a1a
Fouad Hamdi <fhamdi@vmware.com>
2022-12-09 09:50:00 +0100

    ESBACK-122 Add a plugin interface for backstage kubernetes backend plugin

---
+++ plugins/esback-kubernetes-backend/src/KubernetesBackendPlugin.ts
@@ -0,0 +1,31 @@
+import {
+  BackendPluginInterface,
+  BackendPluginSurface,
+  PluginEnvironment,
+} from '@esback/core';
+
+import { CatalogClient } from '@backstage/catalog-client';
+import { Router } from 'express';
+import {KubernetesBuilder} from '@backstage/plugin-kubernetes-backend';
+
+const createPlugin = () => {
+  return async (env: PluginEnvironment): Promise<Router> => {
+    const catalogApi = new CatalogClient({discoveryApi: env.discovery});
+
+    const { router } = await KubernetesBuilder.createBuilder({
+      logger: env.logger,
+      config: env.config,
+      catalogApi,
+    }).build();
+
+    return router;
+  }
+}
+
+export const KubernetesBackendPlugin: BackendPluginInterface = () => surfaces =>
+  surfaces.applyTo(BackendPluginSurface, surface => {
+    surface.addPlugin({
+      name: 'kubernetes',
+      pluginFn: createPlugin(),
+    });
+  });
