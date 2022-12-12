64dc82a0afeef3cb43c98cdb3eca807e3016281c
Fouad Hamdi <fhamdi@vmware.com>
2022-12-12 15:17:02 +0100

    ESBACK-122 Add integration test for the app-live-view plugin

--- packages/app/cypress/integration/rrv-plugin.ts
+++ packages/app/cypress/integration/rrv-plugin.ts
@@ -1,10 +1,15 @@
 describe('RRV Plugin', () => {
-  it('should render the runtime resources and the pod logs', () => {
+  beforeEach(() => {
     cy.visit('/');
+  });
+
+  it('should render the runtime resources and the pod logs', () => {
     cy.contains(/tap-gui-component/i).click();
     cy.contains(/runtime resources/i).click();
-    cy.contains(/resources \(2\)/i);
-    cy.get('table').contains('tr', 'Pod').children().first().next().click();
+    cy.contains('tr', /pod/i)
+      .children()
+      .contains(/server/i)
+      .click();
     cy.contains(/view pod logs/i).click();
     cy.contains(/view pod details/i).should('be.visible');
     cy.get('[role=row]')
@@ -12,5 +17,23 @@ describe('RRV Plugin', () => {
       .should('have.length.greaterThan', 1)
       .contains('level')
       .should('be.visible');
+  });
+
+  it('should render the app live view', () => {
+    cy.contains(/tanzu-java-web-app/i).click();
+    cy.contains(/runtime resources/i).click();
+    cy.contains('tr', /pod/i)
+      .children()
+      .contains(/tanzu-java-web-app/i)
+      .click();
+    cy.contains(/live view/i).should('be.visible');
+    cy.contains('tr', /application name/i)
+      .children('td')
+      .last()
+      .contains('tanzu-java-web-app');
+    cy.contains('tr', /framework/i)
+      .children('td')
+      .last()
+      .contains(/spring boot/i);
   });
 });
