58829eefaca5c1a2bc68c150b254746943417eb1
Fouad Hamdi <fhamdi@vmware.com>
2022-12-12 12:14:44 +0100

    ESBACK-122 Integrate app-live-view plugin

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -2,5 +2,6 @@ import '@backstage/cli/asset-types';
 import { AppRuntime } from '@esback/runtime';
 import { plugin as helloWorldPlugin } from '@esback/plugin-hello-world';
 import { plugin as rrvPlugin } from '@esback/plugin-rrv-interface';
+import { plugin as appLiveViewPlugin } from '@esback/plugin-app-live-view-interface';
 
-new AppRuntime([helloWorldPlugin(), rrvPlugin()]).render();
+new AppRuntime([helloWorldPlugin(), rrvPlugin(), appLiveViewPlugin()]).render();
---
+++ plugins/esback-app-live-view/src/AppLiveViewPlugin.tsx
@@ -0,0 +1,11 @@
+import {AppPluginInterface, AppRouteSurface} from "@esback/core";
+import {Route} from "react-router";
+import {AppLiveViewPage} from "@esback/plugin-app-live-view"
+
+export const AppLiveViewPlugin: AppPluginInterface = () => context => {
+  context.applyTo(AppRouteSurface, (surface) => {
+    surface.add(
+      <Route path="/app-live-view" element={<AppLiveViewPage />}></Route>
+    )
+  })
+}
