cb4fb560b7ea959044ac74c55a493089f0d118c6
Fouad Hamdi <fhamdi@vmware.com>
2023-01-12 13:38:12 +0100

    ESBACK-140 Configure google user as a guest

--- plugins/esback-google-auth-backend/src/GoogleAuthBackendPlugin.ts
+++ plugins/esback-google-auth-backend/src/GoogleAuthBackendPlugin.ts
@@ -1,7 +1,6 @@
 import { BackendPluginInterface } from '@esback/core';
 import { providers } from '@backstage/plugin-auth-backend';
 import { SignInProviderResolverSurface } from '@esback/plugin-auth-backend';
-import { stringifyEntityRef } from '@backstage/catalog-model';
 
 export const GoogleAuthBackendPlugin: BackendPluginInterface =
   () => surfaceStore => {
@@ -11,23 +10,12 @@ export const GoogleAuthBackendPlugin: BackendPluginInterface =
         signInProviderResolverSurface.add({
           google: providers.google.create({
             signIn: {
-              resolver(info, ctx) {
-                const email = info.profile.email;
-                if (!email) {
-                  throw new Error(
-                    'Google login failed, user profile does not contain an email',
-                  );
-                }
-
-                const userEntityRef = stringifyEntityRef({
-                  kind: 'User',
-                  name: email,
-                });
-
+              resolver(_, ctx) {
+                const userRef = 'user:default/guest';
                 return ctx.issueToken({
                   claims: {
-                    sub: userEntityRef,
-                    ent: [userEntityRef],
+                    sub: userRef,
+                    ent: [userRef],
                   },
                 });
               },
