e949c325ec64b72d1ddea4133bc3cb035b902f62
Fouad Hamdi <fhamdi@vmware.com>
2023-01-13 09:59:20 +0100

    ESBACK-140 Test the presence of the sign in button

--- packages/app/cypress/integration/google-auth-plugin.ts
+++ packages/app/cypress/integration/google-auth-plugin.ts
@@ -1,6 +1,14 @@
 import { Authentication } from '../support/authentication';
 
 describe('Google Auth Plugin', () => {
+  it('should display the sign in button', () => {
+    cy.visit('/');
+    cy.contains(/sign in with google oauth/i);
+    cy.contains('li', /google/i)
+      .contains(/sign in/i)
+      .should('be.visible');
+  });
+
   it('should display user email in the settings', () => {
     Authentication.googleUserALogin();
     cy.visit('/settings');
