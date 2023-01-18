fab7043ce5301d50c03314a3ef32e7813f99e677
Fouad Hamdi <fhamdi@vmware.com>
2023-01-18 14:59:50 +0100

    ESBACK-152 Add gitlab plugin

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -1,6 +1,7 @@
 import '@backstage/cli/asset-types';
 import { AppRuntime } from '@esback/runtime';
 import { plugin as helloWorldPlugin } from '@esback/plugin-hello-world';
+import { plugin as gitlabPlugin } from '@esback/plugin-gitlab-loblaw';
 import { plugin as rrvPlugin } from '@esback/plugin-rrv';
 import { plugin as appLiveViewPlugin } from '@esback/plugin-app-live-view';
 import { plugin as oktaAuthPlugin } from '@esback/plugin-okta-auth';
@@ -15,6 +16,7 @@ import { plugin as guestAuthPlugin } from '@esback/plugin-guest-auth';
 
 new AppRuntime([
   helloWorldPlugin(),
+  gitlabPlugin(),
   rrvPlugin(),
   appLiveViewPlugin(),
   oktaAuthPlugin(),
---
+++ plugins/esback-gitlab-loblaw/src/GitlabPlugin.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { AppPluginInterface, AppRouteSurface } from '@esback/core';
+import { EntityLayout, EntitySwitch } from '@backstage/plugin-catalog';
+import { EntityPageSurface } from '@esback/plugin-catalog';
+import {
+  isGitlabAvailable,
+  EntityGitlabContent,
+  EntityGitlabLanguageCard,
+  EntityGitlabContributorsCard,
+  EntityGitlabMergeRequestsTable,
+  EntityGitlabMergeRequestStatsCard,
+  EntityGitlabPipelinesTable,
+} from '@loblaw/backstage-plugin-gitlab';
+import { Grid } from '@material-ui/core';
+
+export const GitlabPlugin: AppPluginInterface = () => context => {
+  context.applyWithDependency(
+    AppRouteSurface,
+    EntityPageSurface,
+    // @ts-ignore routes that is not used
+    (routes, surface) => {
+      surface.addServicePageTab(
+        <EntityLayout.Route
+          if={isGitlabAvailable}
+          path="/gitlab"
+          title="Gitlab"
+        >
+          <EntityGitlabContent />
+        </EntityLayout.Route>,
+      );
+
+      surface.addOverviewContent(
+        <Grid container spacing={3} alignItems="stretch">
+          <EntitySwitch>
+            <EntitySwitch.Case if={isGitlabAvailable}>
+              <Grid item md={6}>
+                <EntityGitlabContributorsCard />
+                <EntityGitlabLanguageCard />
+                <EntityGitlabMergeRequestStatsCard />
+                <EntityGitlabPipelinesTable />
+                <EntityGitlabMergeRequestsTable />
+              </Grid>
+            </EntitySwitch.Case>
+          </EntitySwitch>
+        </Grid>,
+      );
+    },
+  );
+};
