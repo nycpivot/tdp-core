4b05db550c58c6ffc3a8a83aced621e14cd09eb5
Fouad Hamdi <fhamdi@vmware.com>
2023-01-19 14:03:28 +0100

    ESBACK-152 Add integration tests

---
+++ packages/app/cypress/integration/gitlab-plugin.ts
@@ -0,0 +1,25 @@
+import { Authentication } from '../support/authentication';
+
+describe('Gitlab plugin', () => {
+  beforeEach(() => {
+    Authentication.guestLogin();
+    cy.contains(/simple-app/i).click();
+  });
+  it('should display overview', () => {
+    cy.contains(/contributors/i, { timeout: 10000 }).should('be.visible');
+    cy.contains(/languages/i).should('be.visible');
+    cy.contains(/merge requests statistics/i).should('be.visible');
+    cy.contains(/gitlab pipelines/i).should('be.visible');
+    cy.contains(/gitlab merge request/i).should('be.visible');
+  });
+
+  it('should display info in the Gitlab tab', () => {
+    cy.contains('button', 'Gitlab').click();
+    cy.contains(/contributors/i, { timeout: 10000 }).should('be.visible');
+    cy.contains(/languages/i).should('be.visible');
+    cy.contains(/merge requests statistics/i).should('be.visible');
+    cy.contains(/gitlab pipelines/i).should('be.visible');
+    cy.contains(/gitlab merge request/i).should('be.visible');
+    cy.contains(/gitlab issues/i).should('be.visible');
+  });
+});
