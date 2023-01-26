56b7018ed7cf2e0aa77117db616b79610a82143a
Fouad Hamdi <fhamdi@vmware.com>
2023-01-26 13:55:42 +0100

    ESBACK-83 Add integration tests

---
+++ packages/app/cypress/integration/ldap-plugin.ts
@@ -0,0 +1,32 @@
+import { Authentication } from '../support/authentication';
+
+describe('LDAP Plugin', () => {
+  beforeEach(() => {
+    Authentication.guestLogin();
+  });
+
+  it('should display users', () => {
+    cy.get('[aria-haspopup=listbox]').click();
+    cy.get('[role=listbox]').contains(/user/i).click();
+    cy.contains(/alice/i).should('be.visible');
+    cy.contains(/bob/i).should('be.visible');
+    cy.contains(/carol/i).should('be.visible');
+
+    cy.contains(/alice/i).click();
+    cy.contains(/alice aston/i).should('be.visible');
+    cy.contains(/alice@vmware.com/i).should('be.visible');
+
+    cy.contains(/tanzu-portal-builder/i).click();
+
+    cy.contains(/members/i).should('be.visible');
+    cy.contains(/alice aston/i).should('be.visible');
+    cy.contains(/bob barlow/i).should('be.visible');
+  });
+
+  it('should display groups', () => {
+    cy.get('[aria-haspopup=listbox]').click();
+    cy.get('[role=listbox]').contains(/group/i).click();
+    cy.contains(/tap-gui/i).should('be.visible');
+    cy.contains(/tanzu-portal-builder/i).should('be.visible');
+  });
+});
