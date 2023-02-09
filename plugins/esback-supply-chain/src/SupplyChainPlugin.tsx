39c2766be9c9046dfc13ebb9278419a86777ce60
Juan Ospina <juan.ospina.quintero@gmail.com>
2023-02-09 09:42:46 -0500

    Add SSC plugin

---
+++ plugins/esback-supply-chain/src/SupplyChainPlugin.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route } from "react-router";
+import {AppPluginInterface, AppRouteSurface, SidebarItemSurface} from "@esback/core";
+import {SupplyChainPage} from "@tint/plugin-supply-chain";
+import {SidebarItem} from "@backstage/core-components";
+import AccountTreeIcon from "@material-ui/icons/AccountTree";
+
+export const SupplyChainPlugin: AppPluginInterface = () => context => {
+    context.applyTo(AppRouteSurface, routes => {
+            routes.add(<Route path='/supply-chain' element={<SupplyChainPage/>}/>);
+        }
+    );``
+    context.applyTo(SidebarItemSurface, sidebar =>
+        sidebar.addMainItem(
+            <SidebarItem icon={AccountTreeIcon} to='supply-chain' text='Supply Chain' />,
+        ),
+    );
+}
---
+++ plugins/esback-supply-chain/src/index.ts
@@ -0,0 +1,1 @@
+export { SupplyChainPlugin as plugin } from './SupplyChainPlugin';
