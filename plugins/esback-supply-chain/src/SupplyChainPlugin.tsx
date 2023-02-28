bf33327db5dc3a549bae45fcd948bb3e4d0f2ada
Juan Ospina <juan.ospina.quintero@gmail.com>
2023-02-28 11:15:46 -0500

    Update SupplyChainPlugin.tsx

--- plugins/esback-supply-chain/src/SupplyChainPlugin.tsx
+++ plugins/esback-supply-chain/src/SupplyChainPlugin.tsx
@@ -9,7 +9,7 @@ export const SupplyChainPlugin: AppPluginInterface = () => context => {
     context.applyTo(AppRouteSurface, routes => {
             routes.add(<Route path='/supply-chain' element={<SupplyChainPage/>}/>);
         }
-    );``
+    );
     context.applyTo(SidebarItemSurface, sidebar =>
         sidebar.addMainItem(
             <SidebarItem icon={AccountTreeIcon} to='supply-chain' text='Supply Chain' />,
