a1db39550c295be6ffa26ea151175d60d5b5cdf2
Juan Ospina <juan.ospina.quintero@gmail.com>
2023-03-01 10:23:54 -0500

    Update plugin to react 17

--- plugins/esback-supply-chain/src/SupplyChainPlugin.tsx
+++ plugins/esback-supply-chain/src/SupplyChainPlugin.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Route } from "react-router";
 import {AppPluginInterface, AppRouteSurface, SidebarItemSurface} from "@esback/core";
-import {SupplyChainPage} from "@tint/plugin-supply-chain";
 import {SidebarItem} from "@backstage/core-components";
 import AccountTreeIcon from "@material-ui/icons/AccountTree";
+import {SupplyChainPage} from "@tint/plugin-supply-chain";
 
 export const SupplyChainPlugin: AppPluginInterface = () => context => {
     context.applyTo(AppRouteSurface, routes => {
