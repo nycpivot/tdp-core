0327bc120cefd9cd486854e231c152b034f7de75
Fouad Hamdi <fhamdi@vmware.com>
2023-04-05 11:48:08 +0200

    ESBACK-270 copy frontend public folder to portal output

--- packages/builder/portal.config.js
+++ packages/builder/portal.config.js
@@ -1,17 +1,37 @@
 const path = require('path');
 const fs = require('fs');
 const generate = require('generate-file-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+// Files to copy
+// app-config.yaml
+// tsconfig.json
+
+// File to generate
+// .yarnrc
+// package.json
+// yarn.lock ?
+
+// File to replace if provided as inputs
+// app-config.yaml
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/entrypoint.js'),
   output: {
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'portal'),
   },
   plugins: [
-    generate({
-      file: path.resolve(__dirname, 'dist/backstage.json'),
-      content:
-          fs.readFileSync(path.resolve(__dirname, '../../backstage.json'))
-    })
-  ]
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, '../app/public'),
+          to: 'packages/app/public',
+        },
+        {
+          from: path.resolve(__dirname, '../../backstage.json'),
+          to: 'backstage.json',
+        },
+      ],
+    }),
+  ],
 };
