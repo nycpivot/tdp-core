2ec4ba8002bdf64f5e13d29a6a39fc1923a0f316
Fouad Hamdi <fhamdi@vmware.com>
2023-04-11 13:51:26 +0200

    ESBACK-270 better encapsulate behavior of portal builder

--- packages/builder/portal.config.ts
+++ packages/builder/portal.config.ts
@@ -1,23 +1,22 @@
 import * as path from 'path';
 import * as CopyPlugin from 'copy-webpack-plugin';
 import * as generate from 'generate-file-webpack-plugin';
-import { PortalConfiguration, prepareData } from './src/PortalConfiguration';
+import { PortalConfiguration } from './src/PortalConfiguration';
 
 export default env => {
   const portalConfiguration = PortalConfiguration.fromEnv(env);
-  const data = prepareData(portalConfiguration);
 
   return {
     entry: path.resolve(__dirname, 'src/entrypoint.js'),
     output: {
       path: path.resolve(__dirname, portalConfiguration.outputFolder),
     },
-    mode: portalConfiguration.isProduction ? 'production' : 'development',
+    mode: portalConfiguration.mode,
     plugins: [
       new CopyPlugin({
-        patterns: data.filesToCopy,
+        patterns: portalConfiguration.filesToCopy,
       }),
-      ...data.fileContents.map(f =>
+      ...portalConfiguration.fileGenerators.map(f =>
         generate({ file: f.file, content: f.content }),
       ),
     ],
--- packages/builder/src/PortalConfiguration.ts
+++ packages/builder/src/PortalConfiguration.ts
@@ -3,54 +3,52 @@ import { parse as parseYaml } from 'yaml';
 import * as fs from 'fs';
 import { yarnResolver } from './version_resolver';
 import * as path from 'path';
-import { compile } from 'handlebars';
 
 const resolvePath = file => path.resolve(path.dirname(__filename), file);
 
-const template = file => {
-  return compile(fs.readFileSync(resolvePath(file)).toString());
+function readFileContent(file) {
+  return fs.readFileSync(resolvePath(file)).toString();
+}
+
+type EnvironmentProperties = {
+  app_config: string | undefined;
+  output_folder: string | undefined;
+  tpb_config: string | undefined;
+  yarnrc_folder: string | undefined;
+  production: string | undefined;
 };
 
-const generate = (file, templateFile, config: TpbConfiguration) => {
-  return {
-    file: file,
-    content: () => template(templateFile)(config.resolve()),
-  };
-};
+export class PortalConfiguration {
+  private readonly _isProduction: boolean;
+  private readonly _outputFolder: string;
+  private readonly _appConfigFile: string;
+  private readonly _tpbConfig: TpbConfiguration;
 
-export const prepareData = (portalConfiguration: PortalConfiguration) => {
-  const config = portalConfiguration.tpbConfig;
-  return {
-    fileContents: [
-      {
-        file: '.yarnrc',
-        content: portalConfiguration.isProduction
-          ? fs.readFileSync(resolvePath('assets/.yarnrc'))
-          : fs.readFileSync(resolvePath('../../../.yarnrc')),
-      },
-      generate(
-        'packages/app/src/index.ts',
-        'assets/packages/app/src/index.ts.hbs',
-        config,
-      ),
-      generate(
-        'packages/app/package.json',
-        'assets/packages/app/package.json.hbs',
-        config,
-      ),
+  constructor(
+    isProduction: boolean,
+    configFile: string,
+    outputFolder: string,
+    yarnrcFolder: string,
+    appConfigFile: string,
+  ) {
+    this._isProduction = isProduction;
+    this._outputFolder = outputFolder;
+    this._appConfigFile = appConfigFile;
+    this._tpbConfig = new TpbConfiguration(
+      parseYaml(fs.readFileSync(configFile).toString('utf-8')),
+      yarnResolver(yarnrcFolder),
+    );
+  }
 
-      generate(
-        'packages/backend/src/index.ts',
-        'assets/packages/backend/src/index.ts.hbs',
-        config,
-      ),
-      generate(
-        'packages/backend/package.json',
-        'assets/packages/backend/package.json.hbs',
-        config,
-      ),
-    ],
-    filesToCopy: [
+  get outputFolder() {
+    return this._outputFolder;
+  }
+  get mode() {
+    return this._isProduction ? 'production' : 'development';
+  }
+
+  get filesToCopy() {
+    return [
       {
         from: resolvePath('../../app/.eslintrc.js'),
         to: 'packages/app/.eslintrc.js',
@@ -76,59 +74,49 @@ export const prepareData = (portalConfiguration: PortalConfiguration) => {
         to: 'backstage.json',
       },
       {
-        from: portalConfiguration.appConfigFile,
+        from: this._appConfigFile,
         to: 'app-config.yaml',
       },
-    ],
-  };
-};
-
-type EnvironmentProperties = {
-  app_config: string | undefined;
-  output_folder: string | undefined;
-  tpb_config: string | undefined;
-  yarnrc_folder: string | undefined;
-  production: string | undefined;
-};
-
-export class PortalConfiguration {
-  private readonly _isProduction: boolean;
-  private readonly _configFile: string;
-  private readonly _outputFolder: string;
-  private readonly _yarnrcFolder: string;
-  private readonly _appConfigFile: string;
-
-  constructor(
-    isProduction: boolean,
-    configFile: string,
-    outputFolder: string,
-    yarnrcFolder: string,
-    appConfigFile: string,
-  ) {
-    this._isProduction = isProduction;
-    this._configFile = configFile;
-    this._outputFolder = outputFolder;
-    this._yarnrcFolder = yarnrcFolder;
-    this._appConfigFile = appConfigFile;
+    ];
   }
 
-  get outputFolder() {
-    return this._outputFolder;
-  }
-
-  get isProduction() {
-    return this._isProduction;
-  }
-
-  get tpbConfig() {
-    return new TpbConfiguration(
-      parseYaml(fs.readFileSync(this._configFile).toString('utf-8')),
-      yarnResolver(this._yarnrcFolder),
-    );
-  }
-
-  get appConfigFile() {
-    return this._appConfigFile;
+  get fileGenerators() {
+    return [
+      {
+        file: '.yarnrc',
+        content: this._isProduction
+          ? fs.readFileSync(resolvePath('assets/.yarnrc'))
+          : fs.readFileSync(resolvePath('../../../.yarnrc')),
+      },
+      {
+        file: 'packages/app/src/index.ts',
+        content: () =>
+          this._tpbConfig.generate(
+            readFileContent('assets/packages/app/src/index.ts.hbs'),
+          ),
+      },
+      {
+        file: 'packages/app/package.json',
+        content: () =>
+          this._tpbConfig.generate(
+            readFileContent('assets/packages/app/package.json.hbs'),
+          ),
+      },
+      {
+        file: 'packages/backend/src/index.ts',
+        content: () =>
+          this._tpbConfig.generate(
+            readFileContent('assets/packages/backend/src/index.ts.hbs'),
+          ),
+      },
+      {
+        file: 'packages/backend/package.json',
+        content: () =>
+          this._tpbConfig.generate(
+            readFileContent('assets/packages/backend/package.json.hbs'),
+          ),
+      },
+    ];
   }
 
   static fromEnv(env: EnvironmentProperties) {
