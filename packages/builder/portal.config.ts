9fd360da7f8fb65032e0895d0b38fa48331d302a
Fouad Hamdi <fhamdi@vmware.com>
2023-04-14 09:10:31 +0200

    ESBACK-270 reveal better intent description in building the portal

--- packages/builder/portal.config.ts
+++ packages/builder/portal.config.ts
@@ -2,17 +2,16 @@ import * as path from 'path';
 import CopyPlugin from 'copy-webpack-plugin';
 import createFileWithContent from 'generate-file-webpack-plugin';
 import RemovePlugin from 'remove-files-webpack-plugin';
-import { PortalBundleBuilder } from './src/PortalBundleBuilder';
-import { mapEnvProperties } from './src/PortalConfiguration';
-import { PathResolver } from './src/File';
+import { PortalBundle, PortalBundleBuilder } from './src/PortalBundleBuilder';
+import {
+  mapEnvProperties,
+  PortalConfiguration,
+} from './src/PortalConfiguration';
+import { FilePath } from './src/File';
 import { EnvironmentProperties } from './src/EnvironmentProperties';
-
-const resolvePath: PathResolver = file => path.resolve(__dirname, file);
 
 export default (env: EnvironmentProperties) => {
   const config = mapEnvProperties(env, resolvePath);
-  const builder = new PortalBundleBuilder(config);
-  const bundle = builder.build();
 
   return {
     entry: path.resolve(__dirname, 'src/entrypoint.js'),
@@ -21,34 +20,61 @@ export default (env: EnvironmentProperties) => {
     },
     mode: env.production ? 'production' : 'development',
     plugins: [
-      new CopyPlugin({
-        patterns: [
-          {
-            from: resolvePath('bundle'),
-            to: '',
-          },
-          {
-            from: config.appConfig,
-            to: 'app-config.yaml',
-          },
-        ],
-      }),
-      ...bundle.contentBundle.map(createFileWithContent),
-      new RemovePlugin({
-        after: {
-          root: config.outputFolder,
-          include: ['main.js', '.yarnrc.verdaccio', '.yarnrc.artifactory'],
-          test: [
-            {
-              folder: config.outputFolder,
-              method: filePath => {
-                return new RegExp(/\.hbs$/, 'm').test(filePath);
-              },
-              recursive: true,
-            },
-          ],
-        },
-      }),
+      copyBundle(),
+      includeAppConfig(config),
+      ...applyTemplates(config),
+      cleanup(config),
     ],
   };
 };
+
+function resolvePath(file: FilePath): FilePath {
+  return path.resolve(__dirname, file);
+}
+
+function applyTemplates(config: PortalConfiguration) {
+  const builder = new PortalBundleBuilder(config);
+  const bundle = builder.build();
+
+  return bundle.contentBundle.map(createFileWithContent);
+}
+
+function copyBundle() {
+  return new CopyPlugin({
+    patterns: [
+      {
+        from: resolvePath('bundle'),
+        to: '',
+      },
+    ],
+  });
+}
+
+function includeAppConfig(config: PortalConfiguration) {
+  return new CopyPlugin({
+    patterns: [
+      {
+        from: config.appConfig,
+        to: 'app-config.yaml',
+      },
+    ],
+  });
+}
+
+function cleanup(config: PortalConfiguration) {
+  return new RemovePlugin({
+    after: {
+      root: config.outputFolder,
+      include: ['main.js', '.yarnrc.verdaccio', '.yarnrc.artifactory'],
+      test: [
+        {
+          folder: config.outputFolder,
+          method: filePath => {
+            return new RegExp(/\.hbs$/, 'm').test(filePath);
+          },
+          recursive: true,
+        },
+      ],
+    },
+  });
+}
