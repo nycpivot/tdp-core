a042c322a1cfc1cbdea05eb8d07c46d0ee4ed66c
Fouad Hamdi <fhamdi@vmware.com>
2023-05-10 20:50:14 +0200

    ESBACK-316 remove unused imports

--- packages/runtime/src/appRenderer.tsx
+++ packages/runtime/src/appRenderer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navigate, Route } from 'react-router';
+import { Navigate } from 'react-router';
 import { UserSettingsPage } from '@backstage/plugin-user-settings';
 
 import { AlertDisplay, OAuthRequestDialog } from '@backstage/core-components';
