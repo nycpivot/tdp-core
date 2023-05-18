687916d3e9f0433b6c08487ff2eb102624cef4bb
austinal <austinal@vmware.com>
2023-05-18 15:43:31 -0400

    Abstract out common sign-in resolver functions into the sign-in resolver surface

--- plugins/tpb-github-auth-resolver/src/GithubResolver.ts
+++ plugins/tpb-github-auth-resolver/src/GithubResolver.ts
@@ -1,51 +1,29 @@
-import { stringifyEntityRef } from '@backstage/catalog-model';
-import { NotFoundError } from '@backstage/errors';
 import { BackendPluginInterface } from '@tpb/core';
 import { SignInResolverSurface } from '@tpb/plugin-auth-backend';
-import { AuthResolverContext, GithubOAuthResult, SignInResolver } from '@backstage/plugin-auth-backend';
+import {
+  GithubOAuthResult,
+  SignInResolver,
+} from '@backstage/plugin-auth-backend';
 import { BackstageSignInResult } from '@backstage/plugin-auth-node';
 
 export const GithubResolverPlugin: BackendPluginInterface = () => surfaces => {
-  const GithubSignInResolver: SignInResolver<GithubOAuthResult> =
-  async (info, context): Promise<BackstageSignInResult> => {
-    const { fullProfile } = info.result;
-    const userId = fullProfile.username;
-    if (!userId) {
-      throw new Error('Github login failed, user profile does not contain a username');
-    }
+  const GithubSignInResolver =
+    (
+      signInResolverSurface: SignInResolverSurface,
+    ): SignInResolver<GithubOAuthResult> =>
+    async (info, context): Promise<BackstageSignInResult> => {
+      const { fullProfile } = info.result;
+      const userId = fullProfile.username;
+      if (!userId) {
+        throw new Error(
+          'Github login failed, user profile does not contain a username',
+        );
+      }
 
-    return signInWithName(userId, context);
-  };
-
-  /**
-   * Attempt to match the name with a catalog user for sign in.
-   * If that fails, sign in the user with that name without associating with a catalog user.
-   */
-async function signInWithName(name: string, context: AuthResolverContext): Promise<BackstageSignInResult> {
-  const userEntityRef = stringifyEntityRef({
-    kind: 'User',
-    name: name,
-  });
-
-  try {
-    // we await here so that signInWithName throws in the current `try`
-    return await context.signInWithCatalogUser({
-      entityRef: userEntityRef,
-    });
-  } catch (e) {
-    if (!(e instanceof NotFoundError)) {
-      throw e;
-    }
-    return context.issueToken({
-      claims: {
-        sub: userEntityRef,
-        ent: [userEntityRef],
-      },
-    });
-  }
-}
+      return signInResolverSurface.signInWithName(userId, context);
+    };
 
   surfaces.applyTo(SignInResolverSurface, surface => {
-    surface.add('github', GithubSignInResolver);
+    surface.add('github', GithubSignInResolver(surface));
   });
 };
--- plugins/tpb-gitlab-auth-resolver/src/GitlabResolver.ts
+++ plugins/tpb-gitlab-auth-resolver/src/GitlabResolver.ts
@@ -1,51 +1,24 @@
-import { stringifyEntityRef } from '@backstage/catalog-model';
-import { NotFoundError } from '@backstage/errors';
 import { BackendPluginInterface } from '@tpb/core';
 import { SignInResolverSurface } from '@tpb/plugin-auth-backend';
-import { AuthResolverContext, OAuthResult, SignInResolver } from '@backstage/plugin-auth-backend';
+import { OAuthResult, SignInResolver } from '@backstage/plugin-auth-backend';
 import { BackstageSignInResult } from '@backstage/plugin-auth-node';
 
 export const GitlabResolverPlugin: BackendPluginInterface = () => surfaces => {
-  const GitlabSignInResolver: SignInResolver<OAuthResult> =
-  async (info, context): Promise<BackstageSignInResult> => {
-    const email = info.profile.email;
-    if (!email) {
-      throw new Error('Gitlab login failed, user profile does not contain an email');
-    }
-    return signInWithEmail(email, context);
-  };
-
-/**
- * Attempt to match given email with a catalog user's email for sign in.
- * If that fails, sign in the user without associating with a catalog user.
- */
-async function signInWithEmail(email: string, context: AuthResolverContext): Promise<BackstageSignInResult> {
-  const userEntityRef = stringifyEntityRef({
-    kind: 'User',
-    name: email
-  });
-
-  try {
-    // we await here so that signInWithCatalogUser throws in the current `try`
-    return await context.signInWithCatalogUser({
-      filter: {
-        'spec.profile.email': email,
-      },
-    });
-  } catch (e) {
-    if (!(e instanceof NotFoundError)) {
-      throw e;
-    }
-    return context.issueToken({
-      claims: {
-        sub: userEntityRef,
-        ent: [userEntityRef],
-      },
-    });
-  }
-}
+  const GitlabSignInResolver =
+    (
+      signInResolverSurface: SignInResolverSurface,
+    ): SignInResolver<OAuthResult> =>
+    async (info, context): Promise<BackstageSignInResult> => {
+      const email = info.profile.email;
+      if (!email) {
+        throw new Error(
+          'Gitlab login failed, user profile does not contain an email',
+        );
+      }
+      return signInResolverSurface.signInWithEmail(email, context);
+    };
 
   surfaces.applyTo(SignInResolverSurface, surface => {
-    surface.add('gitlab', GitlabSignInResolver);
+    surface.add('gitlab', GitlabSignInResolver(surface));
   });
 };
