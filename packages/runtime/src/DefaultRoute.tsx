fc84d246c0c05a00027e17f4f019300172afb68c
Greg Weresch <gweresch@vmware.com>
2023-05-25 17:14:02 -0400

    Made DefaultRoute.tsx pretty

--- packages/runtime/src/DefaultRoute.tsx
+++ packages/runtime/src/DefaultRoute.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { configApiRef, useApi } from '@backstage/core-plugin-api';
 import { Navigate } from 'react-router';
-// import { AppRouteSurface } from '@tpb/core';
 
-export const DefaultRoute: React.FunctionComponent<any> = (
-  // routeSurface: AppRouteSurface,
-) => {
-  const route = useApi(configApiRef).getOptionalString('customize.default_route') ??
+export const DefaultRoute: React.FunctionComponent<any> = () => {
+  const route =
+    useApi(configApiRef).getOptionalString('customize.default_route') ??
     'catalog';
-  // routeSurface.setDefault(route);
-  return <Navigate key="/" to={route} replace={true} />;
+  return <Navigate key="/" to={route} replace />;
 };
