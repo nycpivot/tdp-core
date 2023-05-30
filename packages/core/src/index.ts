112375205068a097049a49908f68821d7df24c38
Fouad Hamdi <fhamdi@vmware.com>
2023-05-30 10:04:03 +0200

    ESBACK-331 remove backend code from core package

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,5 @@
 export * from '@tpb/core-common';
+export * from '@tpb/core-backend';
 export * from './surfaces';
 export * from './types';
 export type { PluginEnvironment } from './PluginEnvironment';
--- packages/core/src/surfaces/index.ts
+++ packages/core/src/surfaces/index.ts
@@ -2,8 +2,6 @@ export * from './ApiSurface';
 export * from './AppComponentSurface';
 export * from './AppPluginSurface';
 export * from './AppRouteSurface';
-export * from './BackendPluginSurface';
-export * from './BackendCatalogSurface';
 export * from './SidebarItemSurface';
 export * from './ThemeSurface';
 export * from './SettingsTabsSurface';
