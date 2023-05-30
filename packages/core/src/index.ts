d6b6265d5bdaac4c03437dc5e34965b075885012
Fouad Hamdi <fhamdi@vmware.com>
2023-05-30 11:18:24 +0200

    ESBACK-331 remove frontend elements from core package

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,3 @@
 export * from '@tpb/core-common';
+export * from '@tpb/core-frontend';
 export * from '@tpb/core-backend';
-export * from './surfaces';
-export * from './types';
--- packages/core/src/types.ts
+++
@@ -1,8 +0,0 @@
-import type { TpbPluginInterface } from '@tpb/core-common';
-
-export interface RoutableConfig {
-  path?: string;
-  label?: string;
-}
-
-export type AppPluginInterface<T = {}> = (config?: T) => TpbPluginInterface;
