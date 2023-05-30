8d400f39b4ae8357ea11153998bf37447297d2ad
Fouad Hamdi <fhamdi@vmware.com>
2023-05-30 16:15:32 +0200

    ESBACK-331 replace core package with core-frontend in theme plugins

--- plugins/tpb-clarity-theme/src/Root/Root.tsx
+++ plugins/tpb-clarity-theme/src/Root/Root.tsx
@@ -23,7 +23,7 @@ import {
   withStyles,
 } from '@material-ui/core';
 import { Settings as SidebarSettings } from '@backstage/plugin-user-settings';
-import { SidebarItemSurface } from '@tpb/core';
+import { SidebarItemSurface } from '@tpb/core-frontend';
 import { ToggleFeature } from '@tpb/feature-flags';
 import { ClaritySidebarPage } from '../Sidebar/SidebarPage';
 import { ClaritySidebar } from '../Sidebar/Sidebar';
