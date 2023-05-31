7fa2de718873bf9c310c28b7ffa72dcfeed9f500
Fouad Hamdi <fhamdi@vmware.com>
2023-05-31 09:11:45 +0200

    ESBACK-331 replace core package with frontend or backend in hello world plugins

--- plugins/tpb-hello-world-backend/src/HelloWorldBackendPlugin.ts
+++ plugins/tpb-hello-world-backend/src/HelloWorldBackendPlugin.ts
@@ -1,4 +1,7 @@
-import { BackendCatalogSurface, BackendPluginInterface } from '@tpb/core';
+import {
+  BackendCatalogSurface,
+  BackendPluginInterface,
+} from '@tpb/core-backend';
 import { Router } from 'express';
 
 export const HelloWorldBackendPlugin: BackendPluginInterface = () => surfaces =>
