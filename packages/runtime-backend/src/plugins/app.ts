94783d0b48bca9190c441c9f019c07ff730c07c2
Fouad Hamdi <fhamdi@vmware.com>
2023-05-31 12:02:02 +0200

    ESBACK-331 replace core package with core-backend in backend runtime package

--- packages/runtime-backend/src/plugins/app.ts
+++ packages/runtime-backend/src/plugins/app.ts
@@ -1,6 +1,6 @@
 import { createRouter } from '@backstage/plugin-app-backend';
 import { Router } from 'express';
-import { PluginEnvironment } from '@tpb/core';
+import { PluginEnvironment } from '@tpb/core-backend';
 
 export default async function createPlugin(
   env: PluginEnvironment,
