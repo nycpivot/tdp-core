8310be9e239e338d003d67cc80ef75a6f0e1e629
Chris Gunadi <cgunadi@vmware.com>
2023-06-08 20:08:42 -0400

    bump RRV, k8s-custom-backend and alv
    * this updates backstage deps in k8s-custom-backend
    to get to backstage 1.13.2
    
    [ESBACK-322]

--- plugins/tpb-kubernetes-custom-apis-backend/src/KubernetesCustomApisPlugin.ts
+++ plugins/tpb-kubernetes-custom-apis-backend/src/KubernetesCustomApisPlugin.ts
@@ -1,5 +1,9 @@
 import { CatalogClient } from '@backstage/catalog-client';
 import { createRouter } from '@tpb/k8s-custom-apis-backend';
+import {
+  ServerTokenManager,
+} from '@backstage/backend-common'
+import { ServerPermissionClient } from '@backstage/plugin-permission-node'
 import { Router } from 'express';
 import {
   BackendPluginInterface,
@@ -14,10 +18,15 @@ const createPlugin = () => {
     discovery,
   }: PluginEnvironment): Promise<Router> => {
     const catalogApi = new CatalogClient({ discoveryApi: discovery });
+    const tokenManager = ServerTokenManager.fromConfig(config, {
+      logger,
+    })
+    const permissions = ServerPermissionClient.fromConfig(config, { discovery, tokenManager })
     return createRouter({
       logger,
       config,
       catalogApi,
+      permissions,
     });
   };
 };
