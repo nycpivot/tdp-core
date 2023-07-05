27b644ff6f974f07b881a13cdf2b484c13dcb312
Andres Ossa <aossa@vmware.com>
2023-07-05 13:43:26 -0500

    Add some widgets to homepage

--- plugins/tpb-home/src/home/CustomizableHomePage.tsx
+++ plugins/tpb-home/src/home/CustomizableHomePage.tsx
@@ -2,14 +2,37 @@ import React from 'react';
 import {
   HomePageStarredEntities,
   CustomHomepageGrid,
+  HomePageCompanyLogo,
 } from '@backstage/plugin-home';
+import { HomePageSearchBar } from '@backstage/plugin-search';
+import logo from '../../docs/SigilLogo.png';
 
 function CustomizableHomePage() {
+  const defaultConfig = [
+    {
+      component: <HomePageSearchBar />,
+      x: 0,
+      y: 1,
+      width: 12,
+      height: 1,
+    },
+    {
+      component: <HomePageStarredEntities />,
+      x: 0,
+      y: 1,
+      width: 6,
+      height: 6,
+    },
+  ];
+
   return (
-    <CustomHomepageGrid>
-      <h1>Hello, World</h1>
-      <HomePageStarredEntities />
-    </CustomHomepageGrid>
+    <>
+      <CustomHomepageGrid config={defaultConfig}>
+        <HomePageCompanyLogo logo={<img src={logo} />} />
+        <HomePageSearchBar />
+        <HomePageStarredEntities />
+      </CustomHomepageGrid>
+    </>
   );
 }
 
