fffa17128d1e83b1b6a718b5edb804e6a18adb7b
Andres Ossa <aossa@vmware.com>
2023-07-06 16:50:32 -0500

    Add quicklinks widget

--- plugins/tpb-home/src/home/CustomizableHomePage.tsx
+++ plugins/tpb-home/src/home/CustomizableHomePage.tsx
@@ -3,13 +3,21 @@ import {
   HomePageStarredEntities,
   CustomHomepageGrid,
   HomePageCompanyLogo,
+  HomePageToolkit,
+  Tool,
 } from '@backstage/plugin-home';
 // import { HomePageSearchBar } from '@backstage/plugin-search';
-import logo from '../../docs/SigilLogo.png';
-import { makeStyles } from '@material-ui/core/styles';
-import { SearchContextProvider } from '@backstage/plugin-search-react';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+import { configApiRef, useApi } from '@backstage/core-plugin-api';
 
-const useStyles = makeStyles(() => ({
+interface QuickLink {
+  url: string;
+  label: string;
+  icon?: string;
+}
+
+// Fix for Clarity Theme messing everything.
+const useStyles = makeStyles((theme: Theme) => ({
   content: {
     '& > :first-child': {
       height: 'auto',
@@ -19,44 +27,100 @@ const useStyles = makeStyles(() => ({
       marginBottom: '10px',
     },
   },
+  logo: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  container: {
+    margin: theme.spacing(5, 0),
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  quickLinkImage: {
+    width: '64px',
+    height: '64px',
+  },
 }));
+
+interface QuickLinkIconProps {
+  icon?: string;
+  label: string;
+}
+
+function QuickLinkIcon(props: QuickLinkIconProps) {
+  const { icon, label } = props;
+  const classes = useStyles();
+
+  return (
+    <img
+      src={`data:image/svg+xml;base64,${icon}`}
+      alt={label}
+      className={classes.quickLinkImage}
+    />
+  );
+}
 
 function CustomizableHomePage() {
   const classes = useStyles();
+  const config = useApi(configApiRef);
+  const logo = config.getOptionalString('customize.features.home.logo');
+  const logoSrc = `data:image/svg+xml;base64,${logo}`;
+
+  const quickLinks = (config.get('customize.features.home.quickLinks') ||
+    []) as unknown as QuickLink[];
+  const parsedLinks: Tool[] = quickLinks.map(({ url, label, icon }) => {
+    return {
+      url,
+      label,
+      icon: icon && <QuickLinkIcon icon={icon} label={label} />,
+    };
+  });
+
   const defaultConfig = [
     {
-      component: 'HomePageSearchBar',
+      component: 'HomePageCompanyLogo',
       x: 0,
       y: 0,
       width: 12,
-      height: 5,
+      height: 12,
     },
-    {
-      component: 'HomePageRandomJoke',
-      x: 0,
-      y: 2,
-      width: 6,
-      height: 16,
-    },
+    // {
+    //   component: 'HomePageSearchBar',
+    //   x: 0,
+    //   y: 13,
+    //   width: 12,
+    //   height: 5,
+    // },
     {
       component: 'HomePageStarredEntities',
       x: 0,
-      y: 2,
+      y: 19,
+      width: 6,
+      height: 12,
+    },
+    {
+      component: 'HomePageToolkit',
+      x: 7,
+      y: 19,
       width: 6,
       height: 12,
     },
   ];
 
   return (
-    <SearchContextProvider>
-      <div className={classes.content}>
-        <CustomHomepageGrid config={defaultConfig} rowHeight={10}>
-          <HomePageCompanyLogo logo={<img src={logo} alt="logo" />} />
-          {/* <HomePageSearchBar /> */}
-          <HomePageStarredEntities />
-        </CustomHomepageGrid>
-      </div>
-    </SearchContextProvider>
+    <div className={classes.content}>
+      <CustomHomepageGrid config={defaultConfig} rowHeight={10}>
+        <HomePageCompanyLogo
+          className={classes.container}
+          logo={
+            <img src={logoSrc} alt="Company Logo" className={classes.logo} />
+          }
+        />
+        {/* <HomePageSearchBar /> */}
+        <HomePageStarredEntities />
+        <HomePageToolkit title="Quick Links" tools={parsedLinks} />
+      </CustomHomepageGrid>
+    </div>
   );
 }
 
