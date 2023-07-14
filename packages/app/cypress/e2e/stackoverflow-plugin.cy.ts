4f66a41c90189c337c670dbf7c18620157d727dc
Andres Ossa <aossa@vmware.com>
2023-07-14 13:31:42 -0500

    Improve test with fixtures

--- packages/app/cypress/e2e/stackoverflow-plugin.cy.ts
+++ packages/app/cypress/e2e/stackoverflow-plugin.cy.ts
@@ -4,6 +4,13 @@ describe('Backstage Stack Overflow plugin v0.1.17 in TPB v1.6.0', () => {
   beforeEach(() => {
     localStorage.clear();
     Authentication.logInAsCatalogAdmin();
+    cy.intercept(
+      'GET',
+      'https://api.stackexchange.com/2.2/questions?tagged=backstage&site=stackoverflow&pagesize=5',
+      {
+        fixture: '../fixtures/stackoverflow/questions.json',
+      },
+    ).as('getQuestions');
   });
 
   it('should be allowed to be added using the homepage edit widget', () => {
@@ -18,5 +25,13 @@ describe('Backstage Stack Overflow plugin v0.1.17 in TPB v1.6.0', () => {
     cy.get('button').contains('Restore defaults').click();
     cy.get('button').contains('Save').click();
     cy.get('div').contains('Stack Overflow Questions').should('be.visible');
+
+    cy.wait('@getQuestions');
+    cy.get('li')
+      .contains('Backstage Docker build error in yarn step')
+      .should('be.visible');
+    cy.get('li')
+      .contains('SCMintegration in scafold backend')
+      .should('be.visible');
   });
 });
