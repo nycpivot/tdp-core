238a78c978d8c71bd33821a5f01ab52d8a351350
Juan Ospina <juan.ospina.quintero@gmail.com>
2023-07-18 12:40:57 -0500

    Update gitlab-backend-plugin.cy.ts
    Trigger pipeline

--- packages/app/cypress/e2e/gitlab-backend-plugin.cy.ts
+++ packages/app/cypress/e2e/gitlab-backend-plugin.cy.ts
@@ -6,10 +6,12 @@ describe('Gitlab Backend Plugin', () => {
   });
 
   it('should render the component from the provider', () => {
+    cy.get('input[placeholder=Filter]').type('gitlab-provider-component');
     cy.contains('gitlab-provider-component');
   });
 
   it('should render the component from the processor', () => {
+    cy.get('input[placeholder=Filter]').type('gitlab-processor-component');
     cy.contains('gitlab-processor-component');
   });
 });
