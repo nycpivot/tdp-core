e7bd55fa7e0a251cd055d3de5f06a6532fe6fc6f
Juan Ospina <juan.ospina.quintero@gmail.com>
2023-07-18 11:01:36 -0500

    Fix lint

--- packages/app/cypress/e2e/gitlab-plugin.cy.ts
+++ packages/app/cypress/e2e/gitlab-plugin.cy.ts
@@ -10,14 +10,10 @@ describe('Gitlab plugin', () => {
     cy.contains(/contributors/i, { timeout: 10000 }).should('be.visible');
     cy.contains(/languages/i).should('be.visible');
     cy.contains(/merge requests statistics/i).should('be.visible');
-    // eslint-disable-next-line cypress/unsafe-to-chain-command
-    cy.contains(/gitlab pipelines/i)
-      .scrollIntoView()
-      .should('be.visible');
-    // eslint-disable-next-line cypress/unsafe-to-chain-command
-    cy.contains(/gitlab merge request/i)
-      .scrollIntoView()
-      .should('be.visible');
+    cy.contains(/gitlab pipelines/i).scrollIntoView();
+    cy.contains(/gitlab pipelines/i).should('be.visible');
+    cy.contains(/gitlab merge request/i).scrollIntoView();
+    cy.contains(/gitlab merge request/i).should('be.visible');
   });
 
   it('should display info in the Gitlab tab', () => {
